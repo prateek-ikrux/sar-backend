39605e6d94ea4ecb5e8c2375640422b37ddd3037
Prateek <prateek.r@ikrux.com>
2026-09-21 19:56:00 +0530

    fix: standardize import statements in user and auth middleware files

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,9 @@
-import { asyncHandler } from '../utils/asyncHandler';
-import { ApiError } from '../utils/ApiError';
-import { ApiResponse } from '../utils/ApiResponse';
-import { User } from '../models/user.model.js';
-import jwt from 'jsonwebtoken';
-import mongoose from 'mongoose';
+import asyncHandler from "../utils/asyncHandler.js";
+import ApiError from "../utils/apiError.js";
+import ApiResponse from "../utils/apiResponse.js";
+import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
 const generateAccessToken = asyncHandler(async (userId) => {
     try {
